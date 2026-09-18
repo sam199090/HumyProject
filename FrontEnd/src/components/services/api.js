185c3bd83d87abbc93c495b3a62fb2a1f078ae92
Samer <Anathema37@hotmail.com>
2026-09-18 16:52:46 +0200

    Update API to HTTPS

--- FrontEnd/src/components/services/api.js
+++ FrontEnd/src/components/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://humy-backend.runasp.net/api',
+    baseURL: 'https://humy-backend.runasp.net/api',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
